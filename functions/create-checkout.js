372a97a32373d9b8ecf0ecc71c463ac9a4bf3d83
Ricky Rivas <76985350+RickyRivas@users.noreply.github.com>
2022-01-13 23:28:28 -0600

    Update create-checkout.js

--- functions/create-checkout.js
+++ functions/create-checkout.js
@@ -4,9 +4,7 @@ const serverless = require('serverless-http')
 const stripe = require('stripe')('sk_test_51KHf12AC31TQrdAWF4dtXXzd1DuI26OpkShLsQwCwENmYb4GB1PhX6utzbGb4dgFCNBj9oBQ4YxM4zr14rvTMN2700UBybxiqz')
 const bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({
-    extended: false
-}))
+app.use(bodyParser.json())
 
 app.post('/.netlify/functions/create-checkout', async (req, res) => {
 
