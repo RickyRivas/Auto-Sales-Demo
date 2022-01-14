5ef1dba47ade6348f223e30a0752618329764ce1
Ricky Rivas <76985350+RickyRivas@users.noreply.github.com>
2022-01-13 23:27:30 -0600

    test

--- functions/create-checkout.js
+++ functions/create-checkout.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const app = express();
 const serverless = require('serverless-http')
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+const stripe = require('stripe')('sk_test_51KHf12AC31TQrdAWF4dtXXzd1DuI26OpkShLsQwCwENmYb4GB1PhX6utzbGb4dgFCNBj9oBQ4YxM4zr14rvTMN2700UBybxiqz')
 const bodyParser = require('body-parser');
 
-app.use(bodyParser)
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
 
-app.post('/create-checkout-session', async (req, res) => {
-    res.send(req.body)
+app.post('/.netlify/functions/create-checkout', async (req, res) => {
+
   const session = await stripe.checkout.sessions.create({
     line_items: [
       {
@@ -30,4 +32,4 @@ app.post('/create-checkout-session', async (req, res) => {
 });
 
 
-module.exports.handler = serverless(app)
+module.exports.handler = serverless(app)  
