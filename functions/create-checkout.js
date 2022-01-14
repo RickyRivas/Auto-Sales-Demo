3452c59211deb325ac11d7f3365e7c7f1368ecf6
Ricky Rivas <76985350+RickyRivas@users.noreply.github.com>
2022-01-14 00:19:45 -0600

    Update create-checkout.js

--- functions/create-checkout.js
+++ functions/create-checkout.js
@@ -5,7 +5,7 @@ const serverless = require('serverless-http')
 const stripe = require('stripe')('sk_test_51KHf12AC31TQrdAWF4dtXXzd1DuI26OpkShLsQwCwENmYb4GB1PhX6utzbGb4dgFCNBj9oBQ4YxM4zr14rvTMN2700UBybxiqz')
 const bodyParser = require('body-parser');
 
-
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.post('/.netlify/functions/create-checkout', async (req, res) => {
