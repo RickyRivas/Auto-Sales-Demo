9f990d5e3f789583e6a7a15c2e90a719ba319f47
Ricky Rivas <76985350+RickyRivas@users.noreply.github.com>
2022-01-15 00:05:34 -0600

    animation

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -45,27 +45,38 @@ const vehicles = [{
         desc: 'lorem inpsum decs one tewo three',
         imgs: ['https://naughty-williams-126c1a.netlify.app/assets/teslax.jpeg'],
         id: 1,
+        qty: 1
     },
     {
         title: 'Tesla Model Y',
         price: 99999,
         desc: 'lorem inpsum decs one tewo three',
         imgs: ['https://naughty-williams-126c1a.netlify.app/assets/teslay.jpeg'],
-        id: 2
+        id: 2,
+        qty: 0
     },
     {
         title: 'Tesla Model S',
         price: 54000,
         desc: 'lorem inpsum decs one tewo three',
         imgs: ['https://naughty-williams-126c1a.netlify.app/assets/teslas.jpeg'],
-        id: 3
+        id: 3,
+        qty: 5
     }
 ]
 let result = '';
 const displayVehicles = (vehicles) => {
+
     vehicles.forEach(vehicle => {
         result += `
-        <div class='item'>
+        <div 
+        class='item'
+        data-aos="fade-zoom-in"
+        data-aos-easing="ease-in-back"
+        data-aos-delay="300"
+        data-aos-offset="0"  
+        data-aos-once="true"      
+        >
             <div class='img-container'>
                 <img src='${vehicle.imgs[0]}'>
             </div>
@@ -90,7 +101,7 @@ displayVehicles(vehicles);
 const allCheckoutVehicleBtns = document.querySelectorAll('#checkoutVehicle');
 allCheckoutVehicleBtns.forEach(btn => {
     btn.addEventListener('click', (e) => {
-        const foundProd = vehicles.find(item => item.id === +e.target.dataset.id);
-        createCheckout(foundProd)
+        const foundVeh = vehicles.find(item => item.id === +e.target.dataset.id);
+        createCheckout(foundVeh)
     })
 })
