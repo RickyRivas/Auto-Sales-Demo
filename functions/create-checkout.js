04026f7b067e70477a7aeb580ee37b5608410f73
Ricky Rivas <76985350+RickyRivas@users.noreply.github.com>
2022-01-14 22:55:40 -0600

    Update create-checkout.js

--- functions/create-checkout.js
+++ functions/create-checkout.js
@@ -35,8 +35,52 @@ app.post('/.netlify/functions/create-checkout', async (req, res) => {
     shipping_address_collection: {
       allowed_countries: ['US', 'CA'],
     },
-    success_url: 'https://naughty-williams-126c1a.netlify.app/',
-    cancel_url: 'https://naughty-williams-126c1a.netlify.app/',
+    shipping_options: [
+      {
+        shipping_rate_data: {
+          type: 'fixed_amount',
+          fixed_amount: {
+            amount: 0,
+            currency: 'usd',
+          },
+          display_name: 'Free shipping',
+          // # Delivers between 5-7 business days
+          delivery_estimate: {
+            minimum: {
+              unit: 'business_day',
+              value: 5,
+            },
+            maximum: {
+              unit: 'business_day',
+              value: 7,
+            },
+          }
+        }
+      },
+      {
+        shipping_rate_data: {
+          type: 'fixed_amount',
+          fixed_amount: {
+            amount: 1500,
+            currency: 'usd',
+          },
+          display_name: 'Next day air',
+            //  # Delivers in exactly 1 business day
+          delivery_estimate: {
+            minimum: {
+              unit: 'business_day',
+              value: 1,
+            },
+            maximum: {
+              unit: 'business_day',
+              value: 1,
+            },
+          }
+        }
+      },
+    ],
+      success_url: req.get("origin"),
+      cancel_url: req.get("origin")
   });
 
 
